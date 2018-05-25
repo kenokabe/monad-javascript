52cbdb33c43a953433173d3b95eb520ba64488ef
kenokabe <kenokabe@gmail.com>
2018-05-26 04:01:15 +0900

    commit

--- test.js
+++ test.js
@@ -7,7 +7,10 @@
     throw new TypeError();
   };
 
-  const log = (m) => (typeof m !== 'function')
+  const FUNCTION = 'function';
+  const NUMBER = "number";
+
+  const log = (m) => (typeof m !== FUNCTION)
     ? (() => {
       console.log(m);
       return m;
@@ -15,12 +18,12 @@
     : err();
 
   // return a type checked function
-  const type = s => f => x => (typeof x == s)
+  const type = input => f => x => (typeof x === input)
     ? f(x)
     : err();
 
   //need to define type of args
-  const add1 = type("number")(
+  const add1 = type(NUMBER)(
     a => a + 1
   );
 
@@ -36,8 +39,6 @@
   M([1, 2, 3])(([a, b, c]) => [a + 1, b + 1, c + 1])(log)
 
   M("------")(log);
-
-
   M(10)(add1)(log); //11
   M(10)(add1)(add1)(log); //12
   M(10)(add1)(add1)(add1)(log); //13
@@ -47,15 +48,21 @@
   M(10)(add3)(log); //13
 
   M("------")(log);
-  const plus = (x) => (y => x + y);
+  const plus = type(NUMBER)(
+    (x) => (y => x + y)
+  );
+
   M(plus(1)(5))(log); //6
-  M(5)(M(1)(plus))(log); //6
+  M(1)(plus(5))(log); //6
   const plus1 = M(1)(plus);
   M(5)(plus1)(log); //6
 
   M("------")(log);
-  const map = (f) => (array => array.map(f));
-  const map1 = M(add1)(map);
+  const map = type(FUNCTION)(
+    f => (array => array.map(f))
+  );
+
+  const map1 = M((map)(add1));
   M([1, 2, 3])(log)(map1)(log);
 
   //===
